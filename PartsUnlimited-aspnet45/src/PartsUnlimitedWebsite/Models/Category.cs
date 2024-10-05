d8da5c1ab40a11307938ac32d38d2ca5e957d1d2
Amal Bouazza <amal.bouazza@u-virtuelle.tn>
2024-10-05 18:41:21 +0100

    My commit

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
@@ -3,6 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace PartsUnlimited.Models
 {
+    // My second change.
     public class Category
     {
         public int CategoryId { get; set; }
